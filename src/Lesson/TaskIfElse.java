4d46fb9fe5ef2c0a878843b12af404813741bb82
nazarkupnovytskyi <kupnovytskyinazar@gmail.com>
2022-09-27 20:47:22 +0300

    correction task on theme if else!

--- src/Lesson/TaskIfElse.java
+++ src/Lesson/TaskIfElse.java
@@ -3,28 +3,24 @@ package Lesson;
 public class TaskIfElse {
     public static void main(String[] args) {
         TaskIfElse p1 = new TaskIfElse();
-        System.out.println(p1.getResult(10,5));
+        System.out.println(p1.getResult(1,2));
     }
+        public double getResult(double a, double b) {
 
+            double added = a + b;
+            double subtraction = a - b;
+            double division = a / b;
+            double multiplication = a * b;
 
-    public double getResult(double a, double b) {
-
-        double added = a + b;
-        double subtraction = a - b;
-        double division = a / b;
-        double multiplication = a * b;
-
-        if(added < subtraction){
-            return subtraction;
-        } else if (multiplication < division) {
-            return division;
-        } else if (multiplication>=subtraction) {
-            return multiplication;
+            if (added < subtraction) {
+                return subtraction;
+            } else if (multiplication < division) {
+                return division;
+            } else if (multiplication > added) {
+                return multiplication;
+            } else if (multiplication <= added) {
+                return added;
+            }
+            return getResult(a,b);
         }
-        else {
-            System.out.println(subtraction);
         }
-        return added + subtraction + division + multiplication;
-
-    }
-}
